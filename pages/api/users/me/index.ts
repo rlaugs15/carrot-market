755a4f62529c541ef56a100d18ef4917a5cf95b9
rlaugs15 <68183848+rlaugs15@users.noreply.github.com>
2024-02-01 23:21:53 +0900

    profile 수정 페이지
    - name 수정 추가
    - 백엔드 작업(api/users/me.index.ts)
      email, phone이 user 목록에 존재하면 ok:false와 메세지 반환

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -11,10 +11,67 @@ async function handler(
   res: NextApiResponse<ResponesType>,
   session: IronSession<SessionData>
 ) {
-  const profile = await client.user.findUnique({
-    where: { id: session.user!.id },
-  });
-  return res.json({ ok: true, profile });
+  if (req.method === "GET") {
+    const profile = await client.user.findUnique({
+      where: { id: session.user!.id },
+    });
+    return res.json({ ok: true, profile });
+  }
+  if (req.method === "POST") {
+    const { name, email, phone } = req.body;
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: session.user?.id,
+      },
+    });
+    if (email && email !== currentUser?.email) {
+      const exists = Boolean(
+        await client.user.findUnique({
+          where: { email },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (exists) {
+        return res.json({ ok: false, error: "이미 존재하는 이메일 입니다." });
+      }
+      await client.user.update({
+        where: { id: session.user?.id },
+        data: { email },
+      });
+      return res.json({ ok: true });
+    }
+    if (phone && currentUser?.phone !== phone) {
+      const exists = Boolean(
+        await client.user.findUnique({
+          where: { phone },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (exists) {
+        return res.json({ ok: false, error: "이미 존재하는 번호 입니다." });
+      }
+      await client.user.update({
+        where: { id: session.user?.id },
+        data: { phone },
+      });
+      return res.json({ ok: true });
+    }
+    if (name) {
+      await client.user.update({
+        where: {
+          id: session.user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+    }
+    res.json({ ok: true });
+  }
 }
 
-export default withHandler({ methods: ["GET"], handler });
+export default withHandler({ methods: ["GET", "POST"], handler });
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,11 +2,69 @@ import { NextPage } from "next";
 import Layout from "./../../components/layout";
 import Input from "./../../components/input";
 import Button from "./../../components/button";
+import { useForm } from "react-hook-form";
+import useUser from "@/libs/client/useUser";
+import { useEffect } from "react";
+import useMutation from "@/libs/client/useMutation";
+import { useRouter } from "next/router";
+
+interface EditProfileForm {
+  name?: string;
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
+
+interface EditProfileResponese {
+  ok: boolean;
+  error?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  const [editProfile, { data, loading }] =
+    useMutation<EditProfileResponese>("/api/users/me");
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+    if (user?.name) setValue("name", user.name);
+  }, [user, setValue]);
+
+  const onValid = ({ name, email, phone }: EditProfileForm) => {
+    if (loading) return;
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "email과 phone 중 하나를 입력하세요.",
+      });
+    }
+    /* 각각 email, phone이 유저의 email, phone과 다를 때만 백엔드로 보냄
+    하지만 백엔드에서 하는게 더 좋아보인다.
+    editProfile({
+      email: email !== user?.email ? email : "",
+      phone: phone !== user?.phone ? phone : "",
+    }); */
+    editProfile({ name, email, phone });
+  };
+  const router = useRouter();
+  useEffect(() => {
+    if (data && !data.ok) {
+      setError("formErrors", { message: data.error });
+    }
+    if (data && data.ok) {
+      router.back();
+    }
+  }, [data, router, setError]);
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
@@ -22,15 +80,31 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
+          register={register("name")}
+          label="Name"
+          name="name"
+          type="text"
+        />
+        <Input
+          register={register("email")}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
           label="Phone number"
           name="phone"
           type="number"
           kind="phone"
         />
-        <Button text="Update profile" />
+        {errors.formErrors ? (
+          <span className="text-red-400 font-semibold">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button loading={loading} text="Update profile" />
       </form>
     </Layout>
   );
